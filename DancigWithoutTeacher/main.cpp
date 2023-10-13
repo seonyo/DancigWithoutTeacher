90a0a4239660a023fa634eb9dc90b35fcef552f6
seonyo <jsy6535@naver.com>
2023-10-13 10:01:45 +0900

    ✨[Feat] 메인 배경 이미지 삽입

--- DancigWithoutTeacher/main.cpp
+++ DancigWithoutTeacher/main.cpp
@@ -1,22 +1,39 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(1500, 1000), "선생님 몰래 춤추기");
+	RenderWindow window(sf::VideoMode(1500, 1000), "선생님 몰래 춤추기");
 
-    // 메인 루프
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                // 창을 닫을 때 종료
-                window.close();
-            }
-        }
+	Texture background;
 
-        window.clear(sf::Color::Black);
+	background.loadFromFile("image/background.png");
 
-        window.display();
-    }
+	Sprite backgroundImg(background);
 
-    return 0;
+	// 이미지 크기를 창 크기에 맞게 조절 (비율 계산)
+	backgroundImg.setScale(
+		static_cast<float>(window.getSize().x) / background.getSize().x,
+		static_cast<float>(window.getSize().y) / background.getSize().y
+	);
+
+
+	while (window.isOpen()) {
+		Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == Event::Closed) {
+				window.close();
+			}
+
+		}
+
+		window.clear(Color::Black);
+
+		window.draw(backgroundImg);
+	
+
+		window.display();
+	}
 }
