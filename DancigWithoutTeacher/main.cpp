aeb4a84fb3711ada11c5156a5c7dd2854a7988c7
seonyo <jsy6535@naver.com>
2023-10-13 10:03:01 +0900

    ✨[Feat] 메인 제목 이미지 삽입

--- DancigWithoutTeacher/main.cpp
+++ DancigWithoutTeacher/main.cpp
@@ -8,15 +8,23 @@ int main() {
 	RenderWindow window(sf::VideoMode(1500, 1000), "선생님 몰래 춤추기");
 
 	Texture background;
+	Texture mainContent;
 
 	background.loadFromFile("image/background.png");
+	mainContent.loadFromFile("image/mainContent.png");
 
 	Sprite backgroundImg(background);
+	Sprite mainContentImg(mainContent);
+
 
 	// 이미지 크기를 창 크기에 맞게 조절 (비율 계산)
 	backgroundImg.setScale(
 		static_cast<float>(window.getSize().x) / background.getSize().x,
 		static_cast<float>(window.getSize().y) / background.getSize().y
+	);
+	mainContentImg.setScale(
+		500.0f / mainContent.getSize().x,
+		220.0f / mainContent.getSize().y
 	);
 
 
@@ -32,8 +40,8 @@ int main() {
 		window.clear(Color::Black);
 
 		window.draw(backgroundImg);
+		window.draw(mainContentImg);
 	
-
 		window.display();
 	}
 }
